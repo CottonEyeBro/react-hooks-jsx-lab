0d96bd8f2e2d617dbe1d0d08690f4051e2a5bce6
Cooper <55swimmer55@gmail.com>
2023-09-12 11:49:28 -0400

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I hope this JSX runs correctly!</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  );
 }
 
 export default About;
